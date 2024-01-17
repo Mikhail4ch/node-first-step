8dffd8c671f4f651b94e2d0aab0291e92a2161e1
Mikhail4ch <mihamakarevi426@gmail.com>
2024-01-17 14:37:42 +0300

    correction of mistakes

--- assignment.sql
+++ assignment.sql
@@ -2,35 +2,35 @@
 // tables creation and insertion
 
 CREATE TABLE users (
-id integer NOT null,
+id integer NOT NULL,
 username VARCHAR(255) NOT NULL,
-password_ VARCHAR(255) NOT NULL,
+password VARCHAR(255) NOT NULL,
 email VARCHAR(255) NOT NULL,
-firstName text DEFAULT NOT NULL,
-lastName text DEFAULT NOT NULL,
+firstName text NOT NULL,
+lastName text NOT NULL,
 PRIMARY KEY(id)
 ); 
 
-INSERT INTO users(id, username, password_, email, firstName, lastName)
-VALUES (1, "Goose", 1234, "coolgoose@gmail.com", "John", "Cena" );
+INSERT INTO users(id, username, password, email, firstName, lastName)
+VALUES (1, 'Goose', 1234, 'coolgoose@gmail.com', 'John', 'Cena' );
 
-INSERT INTO users(id, username, password_, email, firstName, lastName)
-VALUES (2, "capybara", 4321, "coolcapybara@gmail.com", "Ryan", "Gosling" );
+INSERT INTO users(id, username, password, email, firstName, lastName)
+VALUES (2, 'capybara', 4321, 'coolcapybara@gmail.com', 'Ryan', 'Gosling' );
 
 CREATE TABLE countries (
 id integer NOT null,
-name_ VARCHAR(255) NOT NULL,
+name VARCHAR(255) NOT NULL,
 PRIMARY KEY(id)
 ); 
 
-INSERT INTO countries(id,name_) VALUES(1, Norway);
-INSERT INTO countries(id,name_) VALUES(2, Sweden);
-INSERT INTO countries(id,name_) VALUES(3, Finland);
+INSERT INTO countries(id,name) VALUES(1, 'Norway');
+INSERT INTO countries(id,name) VALUES(2, 'Sweden');
+INSERT INTO countries(id,name) VALUES(3, 'Finland');
 
 CREATE TABLE addresses (
-id integer NOT null,
-user_id I integer,
-country_id integer,
+id integer NOT NULL,
+user_id I integer NOT NULL,
+country_id integer NOT NULL,
 PRIMARY KEY(id),
 FOREIGN KEY (user_id) REFERENCES users(id),
 FOREIGN KEY (country_id) REFERENCES countries(id)
@@ -48,7 +48,9 @@ SELECT email FROM users;
 
 SELECT firstName FROM users WHERE firstName LIKE '%e%';
 
-SELECT countries.name_ FROM countries
+SELECT countries.name, users.firstName, users.lastName FROM countries
 JOIN addresses ON addresses.country_id = countries.id
 WHERE addresses.user_id = 2
+JOIN users ON users.id = addresses.user_id
+
 
