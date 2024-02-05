17180d52e86c47786e26e82c0486db6586d85002
Mikhail4ch <mihamakarevi426@gmail.com>
2024-02-05 17:49:18 +0300

    deployment

--- utils/connection_2.js
+++ utils/connection_2.js
@@ -1,7 +1,7 @@
 const {Sequelize} = require ('sequelize');
 
-const sequelize = new Sequelize('login', "postgres", "miha50544Ki", {
-    host: "localhost",
+const sequelize = new Sequelize('login_osgg', "onetap_web", "4oZpqfZHwYXfLfCO1fg4yTAb37GJMG8U", {
+    host: "dpg-cn0f6c8l5elc73ejb14g-a",
     dialect: "postgres"
 });
 
