acc52f47a404130023a2357f6790d398f029d5b5
Mikhail4ch <mihamakarevi426@gmail.com>
2024-02-19 15:53:01 +0300

    newWay(3)

--- Routes/refreshRoutes.js
+++ Routes/refreshRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const userController = require('../Controllers/refreshTokenController')
+const  refreshTokenController = require('../Controllers/refreshTokenController')
 
 router.get ('/', refreshTokenController.handleRefreshToken)
 
