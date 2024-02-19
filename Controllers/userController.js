bfb5bf6a82d14f4d6753eaa8fa21139bbe65d2d4
Mikhail4ch <mihamakarevi426@gmail.com>
2024-02-19 20:19:35 +0300

    keepitgoing (

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -46,7 +46,7 @@ exports.loginFunction = async (request, response) => {
             const accessToken = jwt.sign({ id: result.id, email: result.email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '100s' });
             const refreshToken = jwt.sign({ id: result.id, email: result.email }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '5min' });
             response.status(200);
-            result.refreshToken = refreshToken;
+            await Users.update({refreshToken: refreshToken}, {where: {email: request.body.email}})
             response.cookie('jwt', refreshToken, {httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000});
             response.json({accessToken});
         }
