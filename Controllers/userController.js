50f7b81b6fea36c45139cede67fab5a2e38cd737
Mikhail4ch <mihamakarevi426@gmail.com>
2024-02-21 16:26:52 +0300

    yammy

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -46,8 +46,8 @@ exports.loginFunction = async (request, response) => {
         passwordValid = await bcrypt.compare(String(request.body.password), String(result.password))
 
         if (passwordValid) {
-            const accessToken = jwt.sign({ id: result.id, email: result.email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '100s' });
-            const refreshToken = jwt.sign({ id: result.id, email: result.email }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '1d' });
+            const accessToken = jwt.sign({ "id": result.id, "email": result.email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '100s' });
+            const refreshToken = jwt.sign({ "id": result.id, "email": result.email }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '1d' });
             await Users.update({ refreshToken:  refreshToken}, {where: { email: request.body.email } })
             response.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000 });
             response.json({ accessToken });
