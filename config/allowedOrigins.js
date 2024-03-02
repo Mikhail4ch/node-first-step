3ae6baf5a8c12cbb19d7dd762baa9424deb98629
Mikhail4ch <mihamakarevi426@gmail.com>
2024-03-02 18:35:54 +0300

    added netlify to allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'https://dashboard.render.com',
     'http://localhost:3000',
-    'https://mikhail4ch.github.io'
+    'https://norwayweather.netlify.app/'
 ];
 
 module.exports = allowedOrigins;
