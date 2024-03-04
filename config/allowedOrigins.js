bc0bf0fd44b41ffbbb6b9817daee7a0b54506854
Mikhail4ch <mihamakarevi426@gmail.com>
2024-03-04 16:52:56 +0300

    one more origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'https://dashboard.render.com',
     'http://localhost:3000',
-    'https://norwayweather.netlify.app'
+    'https://norwayweather.netlify.app',
+    'https://weatherinnorway.netlify.app/'
 ];
 
 module.exports = allowedOrigins;
