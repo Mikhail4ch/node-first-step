c62abee7138eb0996b07e2dfd7589877d7c11e23
Mikhail4ch <mihamakarevi426@gmail.com>
2024-04-05 14:03:03 +0300

    test fixed

--- modules/remove-even-numbers.test.js
+++ modules/remove-even-numbers.test.js
@@ -1,6 +1,6 @@
 const removeEvenNumber = require('./remove-even-numbers');
 
 it ("should remove even numbers from an array", () => {
-    const answer = removeEvenNumber([1,2,3,4,5,6,7,8,9,10]);
+    const answer = removeEvenNumber([1,2,3,4,5,6,7,8,9,10, 11]);
     expect(answer).toEqual([1, 3, 5, 7, 9, 11])
 });
